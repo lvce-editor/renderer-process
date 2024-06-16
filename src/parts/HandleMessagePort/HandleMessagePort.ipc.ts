3c91e2bf696fbb64af085b4dccb5c77be3ad0e79
Le Vivilet <levivlet@pm.me>
2024-06-16 21:44:47 +0200

    test

---
+++ src/parts/HandleMessagePort/HandleMessagePort.ipc.ts
@@ -0,0 +1,7 @@
+import * as HandleMessagePort from './HandleMessagePort.ts'
+
+export const name = 'HandleMessagePort'
+
+export const Commands = {
+  handleMessagePort: HandleMessagePort.handleMessagePort,
+}
