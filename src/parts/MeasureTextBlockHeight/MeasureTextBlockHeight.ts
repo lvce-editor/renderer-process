40357b3bee6cd94c939e57f0e97a9d1e1ccb7c52
Le Vivilet <levivlet@pm.me>
2024-07-06 23:40:10 +0200

    feature: update measureTextBlockHeight api

--- src/parts/MeasureTextBlockHeight/MeasureTextBlockHeight.ts
+++ src/parts/MeasureTextBlockHeight/MeasureTextBlockHeight.ts
@@ -1,16 +1,20 @@
 import * as Assert from '../Assert/Assert.ts'
 import * as GetElementHeight from '../GetElementHeight/GetElementHeight.ts'
 
-export const measureTextBlockHeight = (text, fontSize, fontFamily, width) => {
+export const measureTextBlockHeight = (text, fontSize, fontFamily, lineHeight, width) => {
   Assert.string(text)
-  Assert.string(fontSize)
+  Assert.number(fontSize)
   Assert.string(fontFamily)
+  Assert.number(width)
+  Assert.string(lineHeight)
   const $Measure = document.createElement('div')
   $Measure.style.width = `${width}px`
   $Measure.style.contain = 'content'
   $Measure.style.position = 'absolute'
   $Measure.style.top = '-9999px'
   $Measure.style.left = '-9999px'
+  $Measure.style.fontSize = `${fontSize}px`
+  $Measure.style.lineHeight = lineHeight
   $Measure.textContent = text
   return GetElementHeight.getElementHeight($Measure)
 }
