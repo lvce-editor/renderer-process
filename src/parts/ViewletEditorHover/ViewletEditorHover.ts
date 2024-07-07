98784f0ad730622e596898dafe286d4f6db003c7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-07-07 11:48:10 +0200

    feature: add hover resize events (#11)

--- src/parts/ViewletEditorHover/ViewletEditorHover.ts
+++ src/parts/ViewletEditorHover/ViewletEditorHover.ts
@@ -1,6 +1,9 @@
 import * as SetBounds from '../SetBounds/SetBounds.ts'
 
-export const setBounds = (state, x, y) => {
+export const setBounds = (state, x, y, width, height) => {
   const { $Viewlet } = state
+  SetBounds.setWidth($Viewlet, width)
   SetBounds.setXAndYTransform($Viewlet, x, -y)
 }
+
+export * as Events from './ViewletEditorHoverEvents.ts'
--- src/parts/ViewletEditorHover/ViewletEditorHoverEvents.ts
+++ src/parts/ViewletEditorHover/ViewletEditorHoverEvents.ts
@@ -1,1 +1,18 @@
-export {}
+import * as FunctionalPointerEvents from '../FunctionalPointerEvents/FunctionalPointerEvents.ts'
+
+export const returnValue = true
+
+export const handleSashPointerDown = FunctionalPointerEvents.create(
+  (event) => {
+    const { clientX, clientY } = event
+    return ['handleSashPointerDown', clientX, clientY]
+  },
+  (event) => {
+    const { clientX, clientY } = event
+    return ['handleSashPointerMove', clientX, clientY]
+  },
+  (event) => {
+    const { clientX, clientY } = event
+    return ['handleSashPointerUp', clientX, clientY]
+  },
+)
