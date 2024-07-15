b9f5cd82b9c302d7777c3dc112b53ffd367c2166
Le Vivilet <levivlet@pm.me>
2024-07-15 22:32:34 +0200

    feature: remove unused ts-ignore

--- src/parts/ComponentUid/ComponentUid.ts
+++ src/parts/ComponentUid/ComponentUid.ts
@@ -1,9 +1,7 @@
-// @ts-ignore
 import { getComponentUidFromEvent, getComponentUid, setComponentUid } from '@lvce-editor/virtual-dom'
 
 export const set = setComponentUid
 
 export const get = getComponentUid
 
-// @ts-ignore
 export const fromEvent = getComponentUidFromEvent
