e5d20bf5bf9a2337a43c580d06f8f160d5fdacf6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-07-25 20:48:46 +0200

    feature: allow setting iframe sandbox (#39)

--- src/parts/ViewletE2eTests/ViewletE2eTests.ts
+++ src/parts/ViewletE2eTests/ViewletE2eTests.ts
@@ -1,11 +1,14 @@
 export * as Events from './ViewletE2eTestsEvents.ts'
 
-export const setIframe = (state, src) => {
+export const setIframe = (state, src, sandbox = []) => {
   const $ExistingIframe = document.querySelector('.E2eTestsIframe')
   if ($ExistingIframe) {
     $ExistingIframe.remove()
   }
   const $Iframe = document.createElement('iframe')
+  for (const element of sandbox) {
+    $Iframe.sandbox.add(element)
+  }
   $Iframe.className = 'E2eTestsIframe'
   $Iframe.src = src
   document.body.append($Iframe)
