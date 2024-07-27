5ea67ad75e3a94dcd4dd99f7396b2114092b917c
Le Vivilet <levivlet@pm.me>
2024-07-27 22:56:34 +0200

    feature: make preview draggable

---
+++ src/parts/GetSashId/GetSashId.ts
@@ -0,0 +1,9 @@
+export const getSashId = ($Target) => {
+  if ($Target.id === 'SashPanel') {
+    return 'Panel'
+  }
+  if ($Target.id === 'SashSideBar') {
+    return 'SideBar'
+  }
+  return ''
+}
