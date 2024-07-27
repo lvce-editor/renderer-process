780539b590cf4f76cfdb7d9ae21cb68265e44be2
Le Vivilet <levivlet@pm.me>
2024-07-27 18:24:37 +0200

    add context menu command

--- src/parts/ViewletE2eTests/ViewletE2eTestsEvents.ts
+++ src/parts/ViewletE2eTests/ViewletE2eTestsEvents.ts
@@ -1,3 +1,5 @@
+import * as Event from '../Event/Event.ts'
+
 export const handleClickAt = (event) => {
   const { clientX, clientY } = event
   return ['handleClickAt', clientX, clientY]
@@ -5,6 +7,12 @@ export const handleClickAt = (event) => {
 
 export const handleLoad = (event) => {
   return ['handleLoad']
+}
+
+export const handleContextMenu = (event) => {
+  Event.preventDefault(event)
+  const { button, clientX, clientY } = event
+  return ['handleContextMenu', button, clientX, clientY]
 }
 
 export const returnValue = true
