7ecbc6dbb0702e4f63111582b4cb40d7eb95a751
Le Vivilet <levivlet@pm.me>
2024-07-27 15:03:13 +0200

    feature: send message port to e2e tests iframe

--- src/parts/ViewletE2eTests/ViewletE2eTests.ts
+++ src/parts/ViewletE2eTests/ViewletE2eTests.ts
@@ -1,4 +1,5 @@
 export * as Events from './ViewletE2eTestsEvents.ts'
+import * as Transferrable from '../Transferrable/Transferrable.ts'
 
 export const setIframe = (state, src, sandbox = []) => {
   const $ExistingIframe = document.querySelector('.E2eTestsIframe')
@@ -11,5 +12,25 @@ export const setIframe = (state, src, sandbox = []) => {
   }
   $Iframe.className = 'E2eTestsIframe'
   $Iframe.src = src
+
   document.body.append($Iframe)
+}
+
+export const setPort = (state, portId, origin) => {
+  const $ExistingIframe = document.querySelector('.E2eTestsIframe')
+  if (!$ExistingIframe) {
+    throw new Error('no iframe found')
+  }
+  const port = Transferrable.acquire(portId)
+  // @ts-ignore
+  const { contentWindow } = $ExistingIframe
+  contentWindow.postMessage(
+    {
+      jsonrpc: '2.0',
+      method: 'handleIpc',
+      params: [port],
+    },
+    [port],
+    origin,
+  )
 }
