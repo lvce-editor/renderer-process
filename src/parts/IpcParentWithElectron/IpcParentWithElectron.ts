8c317a62438324b41e0a0629f8a17ca4e2ad874b
Le Vivilet <levivlet@pm.me>
2024-08-24 13:49:56 +0200

    fix: electron ipc

--- src/parts/IpcParentWithElectron/IpcParentWithElectron.ts
+++ src/parts/IpcParentWithElectron/IpcParentWithElectron.ts
@@ -1,5 +1,5 @@
 import * as HandleIpcOnce from '../HandleIpcOnce/HandleIpcOnce.ts'
-import { IpcChildWithWindow } from '@lvce-editor/ipc/dist/browser.js'
+import { IpcChildWithElectronWindow } from '@lvce-editor/ipc/dist/browser.js'
 import * as Assert from '../Assert/Assert.ts'
 import * as IsElectron from '../IsElectron/IsElectron.ts'
 import * as JsonRpc from '../JsonRpc/JsonRpc.ts'
@@ -10,7 +10,7 @@ export const create = async ({ port, ipcId }) => {
   if (!IsElectron.isElectron) {
     throw new Error('Electron api was requested but is not available')
   }
-  const windowIpc = IpcChildWithWindow.wrap(window)
+  const windowIpc = IpcChildWithElectronWindow.wrap(window)
   HandleIpcOnce.handleIpcOnce(windowIpc)
   const webContentsIds = await JsonRpc.invokeAndTransfer(windowIpc, 'CreateMessagePort.createMessagePort', ipcId, port)
   return webContentsIds
