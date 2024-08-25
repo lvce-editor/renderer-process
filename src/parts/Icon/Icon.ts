3d5a7a8f6d1d655e967d4f03d0f955d4b539817d
Le Vivilet <levivlet@pm.me>
2024-08-25 20:51:36 +0200

    feature: remove requesting files for error handling to enable stricter content security policy and reduce renderer process file size

--- src/parts/Icon/Icon.ts
+++ src/parts/Icon/Icon.ts
@@ -1,3 +1,4 @@
 import * as AssetDir from '../AssetDir/AssetDir.ts'
 
+// TODO move this to renderer worker
 export const ExtensionDefaultIcon = `${AssetDir.assetDir}/icons/extensionDefaultIcon.png`
