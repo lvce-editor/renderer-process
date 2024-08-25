c17b2a949efa4d0b75e5f5ee12d6a8ca5dbe6156
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-25 22:06:21 +0200

    feature: make transferable first parameter to align with nodejs (#66)

--- test/Transferrable.test.ts
+++ test/Transferrable.test.ts
@@ -7,14 +7,14 @@ import * as Transferrable from '../src/parts/Transferrable/Transferrable.ts'
 test('transfer and acquire', () => {
   const id = 1
   const value = 2
-  Transferrable.transfer(id, value)
+  Transferrable.transfer(value, id)
   expect(Transferrable.acquire(id)).toBe(value)
 })
 
 test('item should be deleted after acquiring', () => {
   const id = 1
   const value = 2
-  Transferrable.transfer(id, value)
+  Transferrable.transfer(value, id)
   Transferrable.acquire(id)
   expect(Transferrable.acquire(id)).toBe(undefined)
 })
