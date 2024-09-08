636a5b7b4b73e57ecc1556976d0f43dd93484d1a
Le Vivilet <levivlet@pm.me>
2024-09-08 13:08:14 +0200

    feature: allow launch renderer worker with platform web when running in remote

--- src/parts/LaunchRendererWorker/LaunchRendererWorker.ts
+++ src/parts/LaunchRendererWorker/LaunchRendererWorker.ts
@@ -1,9 +1,22 @@
-import * as IsElectron from '../IsElectron/IsElectron.ts'
 import * as LaunchWorker from '../LaunchWorker/LaunchWorker.ts'
+import * as Platform from '../Platform/Platform.ts'
+import * as PlatformType from '../PlatformType/PlatformType.ts'
 import * as RendererWorkerUrl from '../RendererWorkerUrl/RendererWorkerUrl.ts'
 
+const getName = (platform: number) => {
+  switch (platform) {
+    case PlatformType.Electron:
+      return 'Renderer Worker (Electron)'
+    case PlatformType.Web:
+      return 'Renderer Worker (Web)'
+    default:
+      return 'Renderer Worker'
+  }
+}
+
 export const launchRendererWorker = async () => {
-  const name = IsElectron.isElectron ? 'Renderer Worker (Electron)' : 'Renderer Worker'
+  const name = getName(Platform.platform)
+  console.log({ name, p: Platform.platform, s: location.search })
   return LaunchWorker.launchWorker({
     name,
     url: RendererWorkerUrl.rendererWorkerUrl,
