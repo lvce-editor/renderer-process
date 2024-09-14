cc5476bd7ebdc8661f4b78341b6e2cfe916be0b4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-14 09:28:15 +0200

    feature: allow passing credentialless attribute to iframe (#77)

---
+++ src/parts/SetIframeSrc/SetIframeSrc.ts
@@ -0,0 +1,7 @@
+export const setIframeSrc = ($Iframe: HTMLIFrameElement, src: string, srcDoc: string): void => {
+  if (src) {
+    $Iframe.src = src
+  } else if (srcDoc) {
+    $Iframe.srcdoc = srcDoc
+  }
+}
