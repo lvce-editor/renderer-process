2c31a3c80155a27b8eb004aac10f7c399295622c
Le Vivilet <levivlet@pm.me>
2024-09-14 23:46:59 +0200

    feature: create port for worker on launch

--- src/parts/LaunchSyntaxHighlightingWorker/LaunchSyntaxHighlightingWorker.ts
+++ src/parts/LaunchSyntaxHighlightingWorker/LaunchSyntaxHighlightingWorker.ts
@@ -1,9 +1,13 @@
-import * as LaunchWorker from '../LaunchWorker/LaunchWorker.ts'
+import * as IpcParent from '../IpcParent/IpcParent.ts'
+import * as IpcParentType from '../IpcParentType/IpcParentType.ts'
 import * as SyntaxHighlightingWorkerUrl from '../SyntaxHighlightingWorkerUrl/SyntaxHighlightingWorkerUrl.ts'
 
-export const launchSyntaxHighlightingWorker = async () => {
-  return LaunchWorker.launchWorker({
+export const launchSyntaxHighlightingWorker = async (port: MessagePort) => {
+  const ipc = await IpcParent.create({
     name: 'Syntax Highlighting Worker',
     url: SyntaxHighlightingWorkerUrl.syntaxHighlightingWorkerUrl,
+    method: IpcParentType.ModuleWorkerWithMessagePort,
+    port,
   })
+  return ipc
 }
