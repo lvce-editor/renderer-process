512e31099377489755c5ac06d0a1eff964b7518a
Le Vivilet <levivlet@pm.me>
2024-09-14 23:48:44 +0200

    update

--- src/parts/LaunchExtensionHostWorker/LaunchExtensionHostWorker.ts
+++ src/parts/LaunchExtensionHostWorker/LaunchExtensionHostWorker.ts
@@ -1,9 +1,13 @@
 import * as ExtensionHostWorkerUrl from '../ExtensionHostWorkerUrl/ExtensionHostWorkerUrl.ts'
-import * as LaunchWorker from '../LaunchWorker/LaunchWorker.ts'
+import * as IpcParent from '../IpcParent/IpcParent.ts'
+import * as IpcParentType from '../IpcParentType/IpcParentType.ts'
 
-export const launchExtensionHostWorker = async () => {
-  return LaunchWorker.launchWorker({
+export const launchExtensionHostWorker = async (port: MessagePort) => {
+  const ipc = await IpcParent.create({
     name: 'Extension Host Worker',
     url: ExtensionHostWorkerUrl.extensionHostWorkerUrl,
+    method: IpcParentType.ModuleWorkerWithMessagePort,
+    port,
   })
+  return ipc
 }
