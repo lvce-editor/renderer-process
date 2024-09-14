3b1256ab4cce6b6943ae60e0f82e5db52ab02446
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-14 23:38:05 +0200

    feature: use shared ipc states (#92)

--- src/parts/EditorWorker/EditorWorker.ts
+++ src/parts/EditorWorker/EditorWorker.ts
@@ -1,15 +1,8 @@
+import * as IpcStates from '../IpcStates/IpcStates.ts'
 import * as LaunchEditorWorker from '../LaunchEditorWorker/LaunchEditorWorker.ts'
-
-export const state = {
-  /**
-   * @type {any}
-   */
-  ipc: undefined,
-}
 
 export const hydrate = async () => {
   // TODO only launch port and send to renderer worker
   const ipc = await LaunchEditorWorker.launchEditorWorker()
-  // @ts-expect-error
-  state.ipc = ipc
+  IpcStates.set('Editor Worker', ipc)
 }
--- src/parts/SyntaxHighlightingWorker/SyntaxHighlightingWorker.ts
+++ src/parts/SyntaxHighlightingWorker/SyntaxHighlightingWorker.ts
@@ -1,15 +1,8 @@
+import * as IpcStates from '../IpcStates/IpcStates.ts'
 import * as LaunchSyntaxHighlightingWorker from '../LaunchSyntaxHighlightingWorker/LaunchSyntaxHighlightingWorker.ts'
-
-export const state = {
-  /**
-   * @type {any}
-   */
-  ipc: undefined,
-}
 
 export const hydrate = async () => {
   // TODO only launch port and send to renderer worker
   const ipc = await LaunchSyntaxHighlightingWorker.launchSyntaxHighlightingWorker()
-  // @ts-expect-error
-  state.ipc = ipc
+  IpcStates.set('Syntax Highlighting Worker', ipc)
 }
