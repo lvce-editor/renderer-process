3536590c1ccc2af6d48685bd1eddfc15bc2022e2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-14 20:31:35 +0200

    feature: add worker urls (#78)

---
+++ src/parts/SyntaxHighlightingWorkerUrl/SyntaxHighlightingWorkerUrl.ts
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.ts'
+
+export const syntaxHighlightingWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/syntax-highlighting-worker/dist/syntaxHighlightingWorkerMain.js`
