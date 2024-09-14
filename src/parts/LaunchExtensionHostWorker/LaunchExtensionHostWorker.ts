b3055b878154aa09ea8f1a099ffb877ae2cf983f
Le Vivilet <levivlet@pm.me>
2024-09-15 00:17:00 +0200

    feature: send messageport to renderer worker of prelaunched workers

--- src/parts/LaunchExtensionHostWorker/LaunchExtensionHostWorker.ts
+++ src/parts/LaunchExtensionHostWorker/LaunchExtensionHostWorker.ts
@@ -1,10 +1,12 @@
 import * as ExtensionHostWorkerUrl from '../ExtensionHostWorkerUrl/ExtensionHostWorkerUrl.ts'
 import * as IpcParent from '../IpcParent/IpcParent.ts'
 import * as IpcParentType from '../IpcParentType/IpcParentType.ts'
+import * as IsElectron from '../IsElectron/IsElectron.ts'
 
 export const launchExtensionHostWorker = async (port: MessagePort) => {
+  const name = IsElectron.isElectron ? 'Extension Host (Electron)' : 'Extension Host'
   const ipc = await IpcParent.create({
-    name: 'Extension Host Worker',
+    name,
     url: ExtensionHostWorkerUrl.extensionHostWorkerUrl,
     method: IpcParentType.ModuleWorkerWithMessagePort,
     port,
