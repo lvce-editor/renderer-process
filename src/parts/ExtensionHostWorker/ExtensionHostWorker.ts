69c167b3cf0f448e2b7a83fcc0f3ff67c98f2a42
Le Vivilet <levivlet@pm.me>
2024-09-14 23:36:40 +0200

    feature: use shared ipc states

--- src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
+++ src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
@@ -1,15 +1,8 @@
+import * as IpcStates from '../IpcStates/IpcStates.ts'
 import * as LaunchExtensionHostWorker from '../LaunchExtensionHostWorker/LaunchExtensionHostWorker.ts'
-
-export const state = {
-  /**
-   * @type {any}
-   */
-  ipc: undefined,
-}
 
 export const hydrate = async () => {
   // TODO only launch port and send to renderer worker
   const ipc = await LaunchExtensionHostWorker.launchExtensionHostWorker()
-  // @ts-expect-error
-  state.ipc = ipc
+  IpcStates.set('Extension Host Worker', ipc)
 }
