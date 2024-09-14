e2f9679e70109724adf18049f4a880f4a56532a2
Le Vivilet <levivlet@pm.me>
2024-09-14 20:53:17 +0200

    feature: replace config url parameter with json script

--- src/parts/HasFlag/HasFlag.ts
+++ src/parts/HasFlag/HasFlag.ts
@@ -1,7 +1,15 @@
 export const hasFlag = (key: string) => {
-  if (typeof location === 'undefined') {
+  if (typeof location === 'undefined' || typeof document === 'undefined') {
     return false
   }
-  const params = new URLSearchParams(location.search)
-  return params.has(key)
+  const configElement = document.getElementById('Config')
+  if (!configElement) {
+    return false
+  }
+  const text = configElement.textContent
+  if (!text) {
+    return false
+  }
+  const config = JSON.parse(text)
+  return config[key]
 }
