db2a5310685a97a2b940510be43dbfd9d21a08ec
Le Vivilet <levivlet@pm.me>
2024-09-14 23:28:11 +0200

    feature: move shouldLaunchMultipleWorkers variable into separate file

--- src/parts/LaunchWorkers/LaunchWorkers.ts
+++ src/parts/LaunchWorkers/LaunchWorkers.ts
@@ -1,13 +1,11 @@
 import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
 import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
-import * as HasFlag from '../HasFlag/HasFlag.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
+import * as ShouldLaunchMultipleWorkers from '../ShouldLaunchMultipleWorkers/ShouldLaunchMultipleWorkers.ts'
 import * as SyntaxHighlightingWorker from '../SyntaxHighlightingWorker/SyntaxHighlightingWorker.ts'
 
-const launchMultipleWorkers = HasFlag.hasFlag('prelaunchWorkers')
-
 export const launchWorkers = () => {
-  if (launchMultipleWorkers) {
+  if (ShouldLaunchMultipleWorkers.shouldLaunchMultipleWorkers) {
     return Promise.all([RendererWorker.hydrate(), EditorWorker.hydrate(), SyntaxHighlightingWorker.hydrate(), ExtensionHostWorker.hydrate()])
   }
   return RendererWorker.hydrate()
