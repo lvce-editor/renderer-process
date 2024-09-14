a2a19773c74afc12ab8005e8b239df502f1e4f42
Le Vivilet <levivlet@pm.me>
2024-09-14 23:55:06 +0200

    feature: send shouldLaunchMultipleWorkers config to renderer worker

--- src/parts/InitData/InitData.ts
+++ src/parts/InitData/InitData.ts
@@ -1,5 +1,6 @@
-import * as Location from '../Location/Location.ts'
 import * as Layout from '../Layout/Layout.ts'
+import * as Location from '../Location/Location.ts'
+import * as ShouldLaunchMultipleWorkers from '../ShouldLaunchMultipleWorkers/ShouldLaunchMultipleWorkers.ts'
 
 export const getInitData = () => {
   const initData = {
@@ -8,6 +9,9 @@ export const getInitData = () => {
     },
     Layout: {
       bounds: Layout.getBounds(),
+    },
+    Config: {
+      shouldLaunchMultipleWorkers: ShouldLaunchMultipleWorkers.shouldLaunchMultipleWorkers,
     },
   }
   return initData
