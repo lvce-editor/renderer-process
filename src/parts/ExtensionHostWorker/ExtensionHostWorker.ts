cb14784af1d024cf424012787aae7d4e0ace18dd
Le Vivilet <levivlet@pm.me>
2024-09-14 23:49:14 +0200

    launch

--- src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
+++ src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
@@ -2,7 +2,8 @@ import * as IpcStates from '../IpcStates/IpcStates.ts'
 import * as LaunchExtensionHostWorker from '../LaunchExtensionHostWorker/LaunchExtensionHostWorker.ts'
 
 export const hydrate = async () => {
+  const { port1, port2 } = new MessageChannel()
+  IpcStates.set('Extension Host Worker', port2)
   // TODO only launch port and send to renderer worker
-  const ipc = await LaunchExtensionHostWorker.launchExtensionHostWorker()
-  IpcStates.set('Extension Host Worker', ipc)
+  await LaunchExtensionHostWorker.launchExtensionHostWorker(port1)
 }
