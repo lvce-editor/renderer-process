e703808e603018427cbc811d6aef179f8cbcb9ee
Le Vivilet <levivlet@pm.me>
2024-09-14 20:43:35 +0200

    add worker

--- src/parts/LaunchWorkers/LaunchWorkers.ts
+++ src/parts/LaunchWorkers/LaunchWorkers.ts
@@ -1,4 +1,5 @@
 import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
+import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
 import * as HasFlag from '../HasFlag/HasFlag.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 import * as SyntaxHighlightingWorker from '../SyntaxHighlightingWorker/SyntaxHighlightingWorker.ts'
@@ -7,7 +8,7 @@ const launchMultipleWorkers = HasFlag.hasFlag('prelaunchWorkers')
 
 export const launchWorkers = () => {
   if (launchMultipleWorkers) {
-    return Promise.all([RendererWorker.hydrate(), EditorWorker.hydrate(), SyntaxHighlightingWorker.hydrate()])
+    return Promise.all([RendererWorker.hydrate(), EditorWorker.hydrate(), SyntaxHighlightingWorker.hydrate(), ExtensionHostWorker.hydrate()])
   }
   return RendererWorker.hydrate()
 }
