324a96a39eb8aed42b3ea45f392fd46ccf586d7c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-14 23:33:11 +0200

    feature: add ipcStates file (#91)

--- src/parts/IpcParent/IpcParent.ts
+++ src/parts/IpcParent/IpcParent.ts
@@ -1,6 +1,11 @@
 import * as IpcParentModule from '../IpcParentModule/IpcParentModule.ts'
+import * as ShouldLaunchMultipleWorkers from '../ShouldLaunchMultipleWorkers/ShouldLaunchMultipleWorkers.ts'
+import * as IpcStates from '../IpcStates/IpcStates.ts'
 
 export const create = async ({ method, ...options }) => {
+  if (ShouldLaunchMultipleWorkers.shouldLaunchMultipleWorkers && options.name && IpcStates.has(options.name)) {
+    return IpcStates.get(options.name)
+  }
   const module = await IpcParentModule.getModule(method)
   // @ts-ignore
   return module.create(options)
