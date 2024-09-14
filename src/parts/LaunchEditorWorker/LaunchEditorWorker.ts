8b466eab3bffbbf1a57531b6ac20d9978fe9a52e
Le Vivilet <levivlet@pm.me>
2024-09-14 23:48:13 +0200

    update

--- src/parts/LaunchEditorWorker/LaunchEditorWorker.ts
+++ src/parts/LaunchEditorWorker/LaunchEditorWorker.ts
@@ -1,9 +1,13 @@
 import * as EditorWorkerUrl from '../EditorWorkerUrl/EditorWorkerUrl.ts'
-import * as LaunchWorker from '../LaunchWorker/LaunchWorker.ts'
+import * as IpcParent from '../IpcParent/IpcParent.ts'
+import * as IpcParentType from '../IpcParentType/IpcParentType.ts'
 
-export const launchEditorWorker = async () => {
-  return LaunchWorker.launchWorker({
+export const launchEditorWorker = async (port: MessagePort) => {
+  const ipc = await IpcParent.create({
     name: 'Editor Worker',
     url: EditorWorkerUrl.editorWorkerUrl,
+    method: IpcParentType.ModuleWorkerWithMessagePort,
+    port,
   })
+  return ipc
 }
