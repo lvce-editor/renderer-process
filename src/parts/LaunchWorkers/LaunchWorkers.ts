bc309228d2849852ee4d7f43e66f8c4505a5ff61
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-14 20:40:50 +0200

    feature: add option to launch syntax highlighting worker (#82)

--- src/parts/LaunchWorkers/LaunchWorkers.ts
+++ src/parts/LaunchWorkers/LaunchWorkers.ts
@@ -1,11 +1,12 @@
 import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
+import * as SyntaxHighlightingWorker from '../SyntaxHighlightingWorker/SyntaxHighlightingWorker.ts'
 
 const launchMultipleWorkers = false
 
 export const launchWorkers = () => {
   if (launchMultipleWorkers) {
-    return Promise.all([RendererWorker.hydrate(), EditorWorker.hydrate()])
+    return Promise.all([RendererWorker.hydrate(), EditorWorker.hydrate(), SyntaxHighlightingWorker.hydrate()])
   }
   return RendererWorker.hydrate()
 }
