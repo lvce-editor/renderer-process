8c301702d10a735df5e344607dd5b7e97fe116f6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-14 20:45:01 +0200

    feature: prelaunch workers if flag is set (#84)

--- src/parts/LaunchWorkers/LaunchWorkers.ts
+++ src/parts/LaunchWorkers/LaunchWorkers.ts
@@ -1,12 +1,14 @@
 import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
+import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
+import * as HasFlag from '../HasFlag/HasFlag.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 import * as SyntaxHighlightingWorker from '../SyntaxHighlightingWorker/SyntaxHighlightingWorker.ts'
 
-const launchMultipleWorkers = false
+const launchMultipleWorkers = HasFlag.hasFlag('prelaunchWorkers')
 
 export const launchWorkers = () => {
   if (launchMultipleWorkers) {
-    return Promise.all([RendererWorker.hydrate(), EditorWorker.hydrate(), SyntaxHighlightingWorker.hydrate()])
+    return Promise.all([RendererWorker.hydrate(), EditorWorker.hydrate(), SyntaxHighlightingWorker.hydrate(), ExtensionHostWorker.hydrate()])
   }
   return RendererWorker.hydrate()
 }
