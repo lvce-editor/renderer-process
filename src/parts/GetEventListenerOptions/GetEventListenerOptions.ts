0bee2de1c9514df2644cc635d89124ac34554f70
Le Vivilet <levivlet@pm.me>
2024-09-15 09:34:47 +0200

    remove log

--- src/parts/GetEventListenerOptions/GetEventListenerOptions.ts
+++ src/parts/GetEventListenerOptions/GetEventListenerOptions.ts
@@ -1,7 +1,6 @@
 import * as DomEventType from '../DomEventType/DomEventType.ts'
 
 export const getEventListenerOptions = (eventName: string) => {
-  // console.log({ eventName })
   switch (eventName) {
     case DomEventType.Wheel:
       return {
