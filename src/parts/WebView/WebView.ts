ccd59d54adeefeeba08e6c6f2c43135e5dc6660f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-21 19:08:40 +0200

    feature: use position absolute for iframe to load iframe before content and allow moving (#108)

--- src/parts/WebView/WebView.ts
+++ src/parts/WebView/WebView.ts
@@ -2,7 +2,6 @@ import * as SetIframeCredentialless from '../SetIframeCredentialless/SetIframeCr
 import * as SetIframeCsp from '../SetIframeCsp/SetIframeCsp.ts'
 import * as SetIframeSandBox from '../SetIframeSandBox/SetIframeSandBox.ts'
 import * as SetIframeSrc from '../SetIframeSrc/SetIframeSrc.ts'
-import * as Transferrable from '../Transferrable/Transferrable.ts'
 import * as WaitForFrameToLoad from '../WaitForFrameToLoad/WaitForFrameToLoad.ts'
 import * as WebViewState from '../WebViewState/WebViewState.ts'
 
@@ -21,18 +20,19 @@ export const create = async (uid: number, src: string, sandbox: readonly string[
 export const load = async (uid: number) => {
   const $Iframe = WebViewState.get(uid)
   const promise = WaitForFrameToLoad.waitForFrameToLoad($Iframe)
-  document.body.append($Iframe)
+  const parent = document.getElementById('Workbench') as HTMLElement
+  parent.append($Iframe)
   await promise
 }
 
 // TODO rename to sendMessage
-export const setPort = (state, portId, origin) => {
-  const port = Transferrable.acquire(portId)
-  const { frame } = state
-  // TODO wait for load in renderer worker
-  // TODO avoid closure
+export const setPort = (uid: number, port: MessagePort, origin: string) => {
+  const $Iframe = WebViewState.get(uid)
   // TODO use jsonrpc invoke
-  const { contentWindow } = frame
+  const { contentWindow } = $Iframe
+  if (!contentWindow) {
+    throw new Error(`content window not found`)
+  }
   contentWindow.postMessage(
     {
       jsonrpc: '2.0',
