027e69cc276c1db9d17acd82277dd5d959013d75
Le Vivilet <levivlet@pm.me>
2024-09-29 22:40:01 +0200

    fix: extension host worker url

--- src/parts/ExtensionHostWorkerUrl/ExtensionHostWorkerUrl.ts
+++ src/parts/ExtensionHostWorkerUrl/ExtensionHostWorkerUrl.ts
@@ -1,3 +1,3 @@
 import * as AssetDir from '../AssetDir/AssetDir.ts'
 
-export const extensionHostWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/extension-host-worker/dist/exensionHostWorkerMain.js`
+export const extensionHostWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/extension-host-worker/dist/extensionHostWorkerMain.js`
