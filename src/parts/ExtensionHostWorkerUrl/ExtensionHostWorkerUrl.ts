977579eb2a30ddfb28c55dbcf35dd69eca3eaa6c
Le Vivilet <levivlet@pm.me>
2024-10-04 16:46:31 +0200

    feature: allow configuring extension host worker url

--- src/parts/ExtensionHostWorkerUrl/ExtensionHostWorkerUrl.ts
+++ src/parts/ExtensionHostWorkerUrl/ExtensionHostWorkerUrl.ts
@@ -1,3 +1,6 @@
 import * as AssetDir from '../AssetDir/AssetDir.ts'
+import * as GetConfiguredExtensionHostWorkerUrl from '../GetConfiguredExtensionHostWorkerUrl/GetConfiguredExtensionHostWorkerUrl.ts'
 
-export const extensionHostWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/extension-host-worker/dist/extensionHostWorkerMain.js`
+export const extensionHostWorkerUrl =
+  GetConfiguredExtensionHostWorkerUrl.getConfiguredExtensionHostWorkerUrl() ||
+  `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/extension-host-worker/dist/extensionHostWorkerMain.js`
