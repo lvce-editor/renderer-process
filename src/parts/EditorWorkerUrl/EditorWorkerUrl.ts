d18fd1b5725c07fe05f4ff7b301905f732325330
Le Vivilet <levivlet@pm.me>
2024-10-06 08:34:29 +0200

    make configurable

--- src/parts/EditorWorkerUrl/EditorWorkerUrl.ts
+++ src/parts/EditorWorkerUrl/EditorWorkerUrl.ts
@@ -1,3 +1,6 @@
 import * as AssetDir from '../AssetDir/AssetDir.ts'
+import * as GetConfiguredEditorWorkerUrl from '../GetConfiguredEditorWorkerUrl/GetConfiguredEditorWorkerUrl.ts'
 
-export const editorWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/editor-worker/dist/editorWorkerMain.js`
+export const editorWorkerUrl =
+  GetConfiguredEditorWorkerUrl.getConfiguredEditorWorkerUrl() ||
+  `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/editor-worker/dist/editorWorkerMain.js`
