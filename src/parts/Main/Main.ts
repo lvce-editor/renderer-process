f19d6b61b59ce31308a66cb531469749a945d05c
Le Vivilet <levivlet@pm.me>
2024-10-06 07:23:35 +0200

    remove unused code

--- src/parts/Main/Main.ts
+++ src/parts/Main/Main.ts
@@ -1,9 +1,6 @@
 import * as Command from '../Command/Command.ts'
-import * as HandleBeforeInstallPrompt from '../HandleBeforeInstallPrompt/HandleBeforeInstallPrompt.ts'
 import * as LaunchWorkers from '../LaunchWorkers/LaunchWorkers.ts'
 import * as Module from '../Module/Module.ts'
-import * as Platform from '../Platform/Platform.ts'
-import * as PlatformType from '../PlatformType/PlatformType.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 import * as ViewletEditorCompletion from '../ViewletEditorCompletion/ViewletEditorCompletion.ts'
 import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.ts'
@@ -15,11 +12,6 @@ export const main = async () => {
   WindowListeners.enable(window)
   ViewletState.state.modules[ViewletModuleId.EditorCompletion] = ViewletEditorCompletion
   Command.setLoad(Module.load)
-  if (Platform.platform === PlatformType.Web) {
-    // disable prompt to download app as pwa
-    // @ts-expect-error
-    window.onbeforeinstallprompt = HandleBeforeInstallPrompt.handleBeforeInstallPrompt
-  }
   // TODO this is discovered very late
   await LaunchWorkers.launchWorkers()
   VirtualDom.setIpc(RendererWorker)
