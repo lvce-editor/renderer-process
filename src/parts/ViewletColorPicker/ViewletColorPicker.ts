18e383ed19d05745922edca08d4c767a15a0310d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-08 21:10:59 +0200

    feature: add more color picker functions to allow creating color pick… (#126)

--- src/parts/ViewletColorPicker/ViewletColorPicker.ts
+++ src/parts/ViewletColorPicker/ViewletColorPicker.ts
@@ -1,4 +1,6 @@
 import * as SetBounds from '../SetBounds/SetBounds.ts'
+import * as VirtualDom from '../VirtualDom/VirtualDom.ts'
+import * as Widget from '../Widget/Widget.ts'
 export * as Events from './ViewletColorPickerEvents.ts'
 
 export const setColor = (state, color) => {
@@ -10,4 +12,17 @@ export const setOffsetX = (state, offsetX) => {
   const { $Viewlet } = state
   const $ColorPickerSliderThumb = $Viewlet.querySelector('.ColorPickerSliderThumb')
   SetBounds.setXAndYTransform($ColorPickerSliderThumb, offsetX, 0)
+}
+
+export const setDom = (state, dom) => {
+  const { $Viewlet } = state
+  const $Root = VirtualDom.render(dom)
+  // @ts-expect-error
+  $Viewlet.replaceChildren(...$Root.firstChild.childNodes)
+  Widget.append($Viewlet)
+}
+
+export const appendWidget = (state) => {
+  const { $Viewlet } = state
+  Widget.append($Viewlet)
 }
