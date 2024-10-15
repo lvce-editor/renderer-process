ea951abba6dd869c9389fedfa034de44c5c04c95
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-15 20:51:31 +0200

    feature: add hover appendWidget function (#142)

--- src/parts/ViewletEditorHover/ViewletEditorHover.ts
+++ src/parts/ViewletEditorHover/ViewletEditorHover.ts
@@ -1,9 +1,22 @@
+import * as RememberFocus from '../RememberFocus/RememberFocus.ts'
 import * as SetBounds from '../SetBounds/SetBounds.ts'
+import * as Widget from '../Widget/Widget.ts'
+import * as ViewletEditorHoverEvents from './ViewletEditorHoverEvents.ts'
 
 export const setBounds = (state, x, y, width, height) => {
   const { $Viewlet } = state
   SetBounds.setWidth($Viewlet, width)
   SetBounds.setXAndYTransform($Viewlet, x, -y)
+}
+
+export const appendWidget = (state) => {
+  const { $Viewlet } = state
+  Widget.append($Viewlet)
+}
+
+export const setDom = (state, dom) => {
+  const { $Viewlet } = state
+  RememberFocus.rememberFocus($Viewlet, dom, ViewletEditorHoverEvents, 0)
 }
 
 export * as Events from './ViewletEditorHoverEvents.ts'
