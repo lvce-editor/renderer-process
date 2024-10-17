70ee6be166a2c2cada99ff06ab2637020c6a6f84
Le Vivilet <levivlet@pm.me>
2024-10-17 21:45:03 +0200

    register fn

--- src/parts/ViewletEditorSourceActions/ViewletEditorSourceActions.ts
+++ src/parts/ViewletEditorSourceActions/ViewletEditorSourceActions.ts
@@ -1,7 +1,19 @@
+import * as RememberFocus from '../RememberFocus/RememberFocus.ts'
 import * as SetBounds from '../SetBounds/SetBounds.ts'
-export * as Events from './ViewletEditorSourceActionsEvents.ts'
+import * as Widget from '../Widget/Widget.ts'
+import * as Events from './ViewletEditorSourceActionsEvents.ts'
 
 export const setBounds = (state, x, y) => {
   const { $Viewlet } = state
   SetBounds.setXAndYTransform($Viewlet, x, -y)
+}
+
+export const appendWidget = (state) => {
+  const { $Viewlet } = state
+  Widget.append($Viewlet)
+}
+
+export const setDom = (state, dom) => {
+  const { $Viewlet } = state
+  RememberFocus.rememberFocus($Viewlet, dom, Events, 0)
 }
