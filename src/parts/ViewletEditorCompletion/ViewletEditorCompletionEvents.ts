fe07bbd01c8146f81ef20b2cf8311004488f826b
Le Vivilet <levivlet@pm.me>
2024-10-20 22:30:25 +0200

    remove unused

--- src/parts/ViewletEditorCompletion/ViewletEditorCompletionEvents.ts
+++ src/parts/ViewletEditorCompletion/ViewletEditorCompletionEvents.ts
@@ -30,8 +30,6 @@ export const handleScrollBarPointerDown = (event) => {
   ViewletEditorCompletionFunctions.handleScrollBarClick(uid, clientY)
 }
 
-export * from '../VirtualListEvents/VirtualListEvents.ts'
-
 export const handleWheel = (event) => {
   const { deltaMode, deltaY } = event
   const uid = ComponentUid.fromEvent(event)
