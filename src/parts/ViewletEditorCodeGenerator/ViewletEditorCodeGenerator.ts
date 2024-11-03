f6bb53a6c9169a3538daf23c9289ae3be5307b80
Le Vivilet <levivlet@pm.me>
2024-11-03 19:42:26 +0100

    feature: add code generator focus command

--- src/parts/ViewletEditorCodeGenerator/ViewletEditorCodeGenerator.ts
+++ src/parts/ViewletEditorCodeGenerator/ViewletEditorCodeGenerator.ts
@@ -1,5 +1,6 @@
 import * as ApplyUidWorkaround from '../ApplyUidWorkaround/ApplyUidWorkaround.ts'
 import * as SetBounds from '../SetBounds/SetBounds.ts'
+import * as FocusSource from '../FocusSource/FocusSource.ts'
 import * as Widget from '../Widget/Widget.ts'
 
 export const setBounds = (state, x, y, width, height) => {
@@ -15,6 +16,23 @@ export const appendWidget = (state) => {
 
 export const dispose = (state) => {
   Widget.remove(state.$Viewlet)
+}
+
+export const focus = (state, key, source) => {
+  if (!key) {
+    return
+  }
+  if (source !== FocusSource.Script) {
+    return
+  }
+  const { $Viewlet } = state
+  const $Element = $Viewlet.querySelector(key)
+  if (!$Element) {
+    console.warn(`element not found: ${key}`)
+    return
+  }
+  $Element.focus()
+  ApplyUidWorkaround.applyUidWorkaround($Viewlet)
 }
 
 export * as Events from './ViewletEditorCodeGeneratorEvents.ts'
