f068f633749905181b2ffbd16b8d53fe8fca3713
Le Vivilet <levivlet@pm.me>
2024-12-08 22:46:59 +0100

    feature: remove unused about code

--- src/parts/ViewletAbout/ViewletAbout.ts
+++ src/parts/ViewletAbout/ViewletAbout.ts
@@ -1,13 +1,1 @@
-export const setFocused = (state, selector) => {
-  if (selector === true) {
-    selector = 'button'
-  }
-  if (!selector) {
-    return
-  }
-  const { $Viewlet } = state
-  const $Focusable = $Viewlet.querySelector(selector)
-  $Focusable.focus()
-}
-
 export const Events = {}
