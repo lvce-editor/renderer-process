9044afffe8e1335e0e72da80e4fdf11efac38263
Le Vivilet <levivlet@pm.me>
2024-12-21 09:09:56 +0100

    fix

--- src/parts/IpcChild/IpcChild.ts
+++ src/parts/IpcChild/IpcChild.ts
@@ -3,7 +3,9 @@ import * as IpcChildModule from '../IpcChildModule/IpcChildModule.ts'
 export const listen = async ({ method, ...options }) => {
   const module = await IpcChildModule.getModule(method)
   const rawIpc = await module.listen(options)
+  // @ts-ignore
   if (module.signal) {
+    // @ts-ignore
     module.signal(rawIpc)
   }
   const ipc = module.wrap(rawIpc)
