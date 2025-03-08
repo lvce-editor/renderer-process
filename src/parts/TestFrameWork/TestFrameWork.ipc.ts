e9256fb8b9e13cc52da6895e928d07bb4f7e913f
Le Vivilet <levivlet@pm.me>
2025-03-08 09:29:56 +0100

    feature: add performAction2 function

--- src/parts/TestFrameWork/TestFrameWork.ipc.ts
+++ src/parts/TestFrameWork/TestFrameWork.ipc.ts
@@ -1,12 +1,13 @@
+import * as PerformAction2 from '../PerformAction2/PerformAction2.ts'
 import * as TestFrameWork from './TestFrameWork.ts'
 
 export const name = 'TestFrameWork'
 
-// prettier-ignore
 export const Commands = {
   checkMultiElementCondition: TestFrameWork.checkMultiElementCondition,
   checkSingleElementCondition: TestFrameWork.checkSingleElementCondition,
   performAction: TestFrameWork.performAction,
+  performAction2: PerformAction2.performAction2,
   performKeyBoardAction: TestFrameWork.performKeyBoardAction,
   showOverlay: TestFrameWork.showOverlay,
 }
