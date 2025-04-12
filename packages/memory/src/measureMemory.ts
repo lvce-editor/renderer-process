5d9f6b6071cde4d1e1d9fe78a625fc070302e401
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-12 13:29:14 +0200

    feature: update memory measure (#289)

--- packages/memory/src/measureMemory.ts
+++ packages/memory/src/measureMemory.ts
@@ -1,11 +1,8 @@
 import { measureMemory } from '@lvce-editor/measure-memory'
-import { instantiations, instantiationsPath, playwrightPath, threshold, workerPath } from './config.ts'
+import { instantiations, instantiationsPath } from './config.ts'
 
 const main = async () => {
   await measureMemory({
-    playwrightPath,
-    workerPath,
-    threshold,
     instantiations,
     instantiationsPath,
   })
