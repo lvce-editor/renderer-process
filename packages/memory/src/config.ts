b63804f7eb2b49cb9b4d8d726b51ad2c2e119791
Le Vivilet <levivlet@pm.me>
2025-04-12 13:41:56 +0200

    feature: increase typescript memory limit

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -3,6 +3,6 @@ import { root } from './root.ts'
 
 export const threshold = 460_000
 
-export const instantiations = 3000
+export const instantiations = 80000
 
 export const instantiationsPath = join(root, 'packages', 'renderer-process')
