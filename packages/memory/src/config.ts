2bf4de9ee0f336bc5220928df3207b1558a7fc17
Le Vivilet <levivlet@pm.me>
2025-04-12 13:26:17 +0200

    feature: update memory measure

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -5,8 +5,4 @@ export const threshold = 460_000
 
 export const instantiations = 3000
 
-export const instantiationsPath = join(root, 'packages', 'explorer-view')
-
-export const workerPath = join(root, '.tmp/dist/dist/explorerViewWorkerMain.js')
-
-export const playwrightPath = new URL('../../e2e/node_modules/playwright/index.mjs', import.meta.url).toString()
+export const instantiationsPath = join(root, 'packages', 'renderer-process')
