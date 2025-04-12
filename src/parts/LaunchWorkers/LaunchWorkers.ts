7ccb7ac3e5cb9ad2ef1de3765d707365e3657089
Le Vivilet <levivlet@pm.me>
2025-04-12 11:48:26 +0200

    feature: simplify worker functions

--- src/parts/LaunchWorkers/LaunchWorkers.ts
+++ src/parts/LaunchWorkers/LaunchWorkers.ts
@@ -4,9 +4,15 @@ import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 import * as ShouldLaunchMultipleWorkers from '../ShouldLaunchMultipleWorkers/ShouldLaunchMultipleWorkers.ts'
 import * as SyntaxHighlightingWorker from '../SyntaxHighlightingWorker/SyntaxHighlightingWorker.ts'
 
+const workerFns = [RendererWorker.hydrate, EditorWorker.hydrate, SyntaxHighlightingWorker.hydrate, ExtensionHostWorker.hydrate]
+
+const call = (fn: () => Promise<void>): Promise<void> => {
+  return fn()
+}
+
 export const launchWorkers = () => {
   if (ShouldLaunchMultipleWorkers.shouldLaunchMultipleWorkers) {
-    return Promise.all([RendererWorker.hydrate(), EditorWorker.hydrate(), SyntaxHighlightingWorker.hydrate(), ExtensionHostWorker.hydrate()])
+    return Promise.all(workerFns.map(call))
   }
   return RendererWorker.hydrate()
 }
