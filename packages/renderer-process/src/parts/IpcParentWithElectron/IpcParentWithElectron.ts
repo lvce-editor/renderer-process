5f015af74238e699f6610e374422a528a8373baa
Le Vivilet <levivlet@pm.me>
2025-07-08 14:19:12 +0200

    feature: simplify electron rpc

--- packages/renderer-process/src/parts/IpcParentWithElectron/IpcParentWithElectron.ts
+++ packages/renderer-process/src/parts/IpcParentWithElectron/IpcParentWithElectron.ts
@@ -1,9 +1,6 @@
-import * as HandleIpcOnce from '../HandleIpcOnce/HandleIpcOnce.ts'
-// @ts-ignore
-import { IpcChildWithElectronWindow } from '@lvce-editor/ipc'
+import { ElectronWindowRpcClient } from '@lvce-editor/rpc'
 import * as Assert from '../Assert/Assert.ts'
 import * as IsElectron from '../IsElectron/IsElectron.ts'
-import * as JsonRpc from '../JsonRpc/JsonRpc.ts'
 
 // TODO use handleIncomingIpc function
 export const create = async ({ port, ipcId }) => {
@@ -11,8 +8,10 @@ export const create = async ({ port, ipcId }) => {
   if (!IsElectron.isElectron) {
     throw new Error('Electron api was requested but is not available')
   }
-  const windowIpc = IpcChildWithElectronWindow.wrap(window)
-  HandleIpcOnce.handleIpcOnce(windowIpc)
-  const webContentsIds = await JsonRpc.invokeAndTransfer(windowIpc, 'CreateMessagePort.createMessagePort', ipcId, port)
+  const rpc = await ElectronWindowRpcClient.create({
+    commandMap: {},
+    window,
+  })
+  const webContentsIds = await rpc.invokeAndTransfer('CreateMessagePort.createMessagePort', ipcId, port)
   return webContentsIds
 }
