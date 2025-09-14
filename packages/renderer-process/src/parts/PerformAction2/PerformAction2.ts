338569db0d625c0c42e6a6384c4fc56fbc0e5c73
Le Vivilet <levivilet@pm.me>
2025-09-14 09:55:49 +0200

    remove unused code

--- packages/renderer-process/src/parts/PerformAction2/PerformAction2.ts
+++ packages/renderer-process/src/parts/PerformAction2/PerformAction2.ts
@@ -1,30 +1,19 @@
 import * as Assert from '../Assert/Assert.ts'
 import * as ElementActions from '../TestFrameWork/ElementActions.ts'
 import * as QuerySelector from '../TestFrameWork/QuerySelector.ts'
-import * as Time from '../Time/Time.ts'
-import * as Timeout from '../Timeout/Timeout.ts'
-
-// TODO this should also come in via options
-const maxTimeout = 2000
 
 export const performAction2 = async (locator, fnName, options) => {
   Assert.object(locator)
   Assert.string(fnName)
   Assert.object(options)
-  const startTime = Time.getTimeStamp()
-  const endTime = startTime + maxTimeout
-  let currentTime = startTime
   const fn = ElementActions[fnName]
-  while (currentTime < endTime) {
-    const element = QuerySelector.querySelectorWithOptions(locator._selector, {
-      hasText: locator._hasText,
-      nth: locator._nth,
-    })
-    if (element) {
-      fn(element, options)
-      return
-    }
-    await Timeout.waitForMutation(100)
-    currentTime = Time.getTimeStamp()
+  const element = QuerySelector.querySelectorWithOptions(locator._selector, {
+    hasText: locator._hasText,
+    nth: locator._nth,
+  })
+  if (!element) {
+    throw new Error(`element not found`)
   }
+  fn(element, options)
+  return
 }
