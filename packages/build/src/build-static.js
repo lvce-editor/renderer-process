3f60e7854a982932dd4ccca4199e982a2b7a908c
Le Vivilet <levivilet@pm.me>
2025-09-26 19:36:52 +0200

    feature: update renderer process path during postinstall

--- packages/build/src/build-static.js
+++ packages/build/src/build-static.js
@@ -1,7 +1,7 @@
+import { cp, readFile, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { pathToFileURL } from 'node:url'
 import { root } from './root.js'
-import { cp } from 'node:fs/promises'
 
 const sharedProcessPath = join(root, 'packages', 'server', 'node_modules', '@lvce-editor', 'shared-process', 'index.js')
 
@@ -10,9 +10,25 @@ const sharedProcessUrl = pathToFileURL(sharedProcessPath).toString()
 const sharedProcess = await import(sharedProcessUrl)
 
 process.env.PATH_PREFIX = '/renderer-process'
-await sharedProcess.exportStatic({
+const { commitHash } = await sharedProcess.exportStatic({
   root,
   extensionPath: '',
 })
+
+export const getRemoteUrl = (path) => {
+  const url = pathToFileURL(path).toString().slice(8)
+  return `/remote/${url}`
+}
+
+const indexHtmlPath = join(root, 'dist', 'index.html')
+
+const content = await readFile(indexHtmlPath, 'utf8')
+const workerPath = join(root, '.tmp/dist/dist/rendererProcessMain.js')
+const remoteUrl = getRemoteUrl(workerPath)
+
+const occurrence = `${remoteUrl}`
+const replacement = `/${commitHash}/packages/renderer-process/dist/rendererProcessMain.js`
+const newContent = content.replace(occurrence, replacement)
+await writeFile(indexHtmlPath, newContent)
 
 await cp(join(root, 'dist'), join(root, '.tmp', 'static'), { recursive: true })
