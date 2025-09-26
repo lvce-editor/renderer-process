d1b906516febc1d3b1e0bcf143cf612cdbd2dbc5
Le Vivilet <levivilet@pm.me>
2025-09-26 19:45:09 +0200

    feature: update renderer process path during postinstall (#476)
    * feature: update renderer process path during postinstall
    
    * disable for now

--- packages/server/src/postinstall.js
+++ packages/server/src/postinstall.js
@@ -1,1 +1,35 @@
-// TODO
+// import { readdir, readFile, writeFile } from 'node:fs/promises'
+// import { dirname, join } from 'node:path'
+// import { fileURLToPath, pathToFileURL } from 'node:url'
+
+// const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// const root = join(__dirname, '..', '..', '..')
+
+// export const getRemoteUrl = (path) => {
+//   const url = pathToFileURL(path).toString().slice(8)
+//   return `/remote/${url}`
+// }
+
+// const nodeModulesPath = join(root, 'packages', 'server', 'node_modules')
+
+// const rendererProcessPath = join(root, '.tmp', 'dist', 'dist', 'rendererProcessMain.js')
+
+// const serverStaticPath = join(nodeModulesPath, '@lvce-editor', 'static-server', 'static')
+
+// const RE_COMMIT_HASH = /^[a-z\d]+$/
+// const isCommitHash = (dirent) => {
+//   return dirent.length === 7 && dirent.match(RE_COMMIT_HASH)
+// }
+
+// const dirents = await readdir(serverStaticPath)
+// const commitHash = dirents.find(isCommitHash) || ''
+// const rendererWorkerMainPath = join(serverStaticPath, 'index.html')
+
+// const content = await readFile(rendererWorkerMainPath, 'utf-8')
+// const remoteUrl = getRemoteUrl(rendererProcessPath)
+// const occurrence = `/${commitHash}/packages/renderer-process/dist/rendererProcessMain.js`
+// const replacement = `${remoteUrl}`
+
+// const newContent = content.replace(occurrence, replacement)
+// await writeFile(rendererWorkerMainPath, newContent)
