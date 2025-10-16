dee55a70148dc8ddde762d8bfba92f942ec50fe7
Le Vivilet <levivilet@pm.me>
2025-10-16 13:13:17 +0200

    feature: export commands from title bar menu bar

--- packages/renderer-process/src/parts/ViewletTitleBar/ViewletTitleBar.ts
+++ packages/renderer-process/src/parts/ViewletTitleBar/ViewletTitleBar.ts
@@ -30,3 +30,5 @@ export const setFocused = (state, isFocused) => {
   const { $Viewlet } = state
   $Viewlet.classList.toggle(activeClassName, isFocused);
 }
+
+export * from '../ViewletTitleBarMenuBar/ViewletTitleBarMenuBar.ts'
