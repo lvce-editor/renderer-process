fa8cde2c13f3909c63ade012de0c5d22b391d3b8
Le Vivilet <levivilet@pm.me>
2025-10-16 15:48:21 +0200

    feature: remove unused title bar code (#490)

--- packages/renderer-process/src/parts/ViewletTitleBar/ViewletTitleBar.ts
+++ packages/renderer-process/src/parts/ViewletTitleBar/ViewletTitleBar.ts
@@ -1,34 +1,8 @@
-import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
-import * as AttachEvents from '../AttachEvents/AttachEvents.ts'
-import * as DomEventType from '../DomEventType/DomEventType.ts'
-import * as ViewletTitleBarEvents from './ViewletTitleBarEvents.ts'
-
-export const create = () => {
-  // TODO set aria label for title bar menu
-  // TODO add tests for aria properties
-  const $Viewlet = document.createElement('div')
-  $Viewlet.id = 'TitleBar'
-  $Viewlet.className = 'Viewlet TitleBar'
-  $Viewlet.ariaLabel = 'Title Bar'
-  $Viewlet.role = AriaRoles.ContentInfo
-  return {
-    $TitleBar: $Viewlet,
-    $Viewlet,
-  }
-}
-
-export const attachEvents = (state) => {
-  const { $Viewlet } = state
-  AttachEvents.attachEvents($Viewlet, {
-    [DomEventType.ContextMenu]: ViewletTitleBarEvents.handleContextMenu,
-  })
-}
-
 const activeClassName = 'TitleBarActive'
 
 export const setFocused = (state, isFocused) => {
   const { $Viewlet } = state
-  $Viewlet.classList.toggle(activeClassName, isFocused);
+  $Viewlet.classList.toggle(activeClassName, isFocused)
 }
 
 export * from '../ViewletTitleBarMenuBar/ViewletTitleBarMenuBar.ts'
