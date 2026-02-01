aadc4552d53471e184b798fafc049ec6521abbd8
Le Vivilet <levivilet@pm.me>
2026-02-01 22:26:51 +0100

    feature: rename prompt to confirm and add prompt2 in ConfirmPrompt (#556)

--- packages/renderer-process/src/parts/ConfirmPrompt/ConfirmPrompt.ipc.ts
+++ packages/renderer-process/src/parts/ConfirmPrompt/ConfirmPrompt.ipc.ts
@@ -3,5 +3,6 @@ import * as ConfirmPrompt from './ConfirmPrompt.ts'
 export const name = 'ConfirmPrompt'
 
 export const Commands = {
-  prompt: ConfirmPrompt.prompt,
+  prompt: ConfirmPrompt.confirm, // TODO bad name
+  prompt2: ConfirmPrompt.prompt2,
 }
