e540f5fe62d3b3123ad5841be316f18ee5c91c60
Le Vivilet <levivilet@pm.me>
2026-02-08 16:30:15 +0100

    feature: remove TitleBar components and related functionality (#582)
    * feature: remove TitleBar components and related functionality
    
    * fix

--- packages/renderer-process/src/parts/ViewletTitleBar/ViewletTitleBar.ts
+++
@@ -1,8 +0,0 @@
-const activeClassName = 'TitleBarActive'
-
-export const setFocused = (state, isFocused) => {
-  const { $Viewlet } = state
-  $Viewlet.classList.toggle(activeClassName, isFocused)
-}
-
-export * from '../ViewletTitleBarMenuBar/ViewletTitleBarMenuBar.ts'
