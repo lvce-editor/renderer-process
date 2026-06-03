f8e832986993622d30069c7b3622cfc140bb95c1
Le Vivilet <levivilet@pm.me>
2026-06-03 18:36:31 +0200

    feature: add getConfig function and tests for getInitData

--- packages/renderer-process/src/parts/InitData/InitData.ts
+++ packages/renderer-process/src/parts/InitData/InitData.ts
@@ -2,9 +2,18 @@ import * as Layout from '../Layout/Layout.ts'
 import * as Location from '../Location/Location.ts'
 import * as ShouldLaunchMultipleWorkers from '../ShouldLaunchMultipleWorkers/ShouldLaunchMultipleWorkers.ts'
 
+const getConfig = () => {
+  const configElement = document.getElementById('Config')
+  if (!configElement?.textContent) {
+    return {}
+  }
+  return JSON.parse(configElement.textContent)
+}
+
 export const getInitData = () => {
   const initData = {
     Config: {
+      ...getConfig(),
       shouldLaunchMultipleWorkers: ShouldLaunchMultipleWorkers.shouldLaunchMultipleWorkers,
     },
     Layout: {
---
+++ packages/renderer-process/test/InitData.test.ts
@@ -0,0 +1,37 @@
+/**
+ * @jest-environment jsdom
+ */
+import { afterEach, expect, test } from '@jest/globals'
+
+const InitData = await import('../src/parts/InitData/InitData.ts')
+
+afterEach(() => {
+  document.body.innerHTML = ''
+  document.head.innerHTML = ''
+})
+
+test('getInitData includes config from html', () => {
+  const config = document.createElement('script')
+  config.id = 'Config'
+  config.type = 'application/json'
+  config.textContent = JSON.stringify({
+    argv: ['--link', 'file:///test/packages/editor-worker/dist/editorWorkerMain.js'],
+    editorWorkerUrl: '/remote/test/packages/editor-worker/dist/editorWorkerMain.js',
+  })
+  document.head.append(config)
+
+  const initData = InitData.getInitData()
+
+  expect(initData.Config).toMatchObject({
+    argv: ['--link', 'file:///test/packages/editor-worker/dist/editorWorkerMain.js'],
+    editorWorkerUrl: '/remote/test/packages/editor-worker/dist/editorWorkerMain.js',
+  })
+})
+
+test('getInitData works without html config', () => {
+  const initData = InitData.getInitData()
+
+  expect(initData.Config).toEqual({
+    shouldLaunchMultipleWorkers: expect.any(Boolean),
+  })
+})
