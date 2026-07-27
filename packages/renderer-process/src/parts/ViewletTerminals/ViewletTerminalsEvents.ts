690a0e3d6b347df8e3115e8688bab1ea93314690
Le Vivilet <levivilet@pm.me>
2026-07-27 16:20:58 +0200

    fix: handle terminal tab actions (#736)

--- packages/renderer-process/src/parts/ViewletTerminals/ViewletTerminalsEvents.ts
+++ packages/renderer-process/src/parts/ViewletTerminals/ViewletTerminalsEvents.ts
@@ -1,10 +1,15 @@
 import * as ComponentUid from '../ComponentUid/ComponentUid.ts'
-import { getNodeIndex } from '../GetNodeIndex/GetNodeIndex.ts'
+import * as GetNodeIndex from '../GetNodeIndex/GetNodeIndex.ts'
 import * as ViewletTerminalsFunctions from './ViewletTerminalsFunctions.ts'
 
 export const handleClickTab = (event) => {
   const uid = ComponentUid.fromEvent(event)
   const { target } = event
-  const index = getNodeIndex(target)
-  ViewletTerminalsFunctions.handleClickTab(uid, index)
+  const tab = target.closest?.('.TerminalTab')
+  if (!tab) {
+    return
+  }
+  const index = GetNodeIndex.getNodeIndex(tab)
+  const shouldDelete = Boolean(target.closest?.('.TerminalTabDeleteButton'))
+  ViewletTerminalsFunctions.handleClickTab(uid, index, shouldDelete)
 }
