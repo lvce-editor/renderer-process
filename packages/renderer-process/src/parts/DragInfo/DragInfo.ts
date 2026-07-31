b7407522d4e1ad33288a15379a0cd36a9a37e44c
Le Vivilet <levivilet@pm.me>
2026-07-31 02:49:17 -0700

    fix: expose current viewlet drag data (#744)
    * fix: expose current viewlet drag data
    
    * fix: store current drag data in state

--- packages/renderer-process/src/parts/DragInfo/DragInfo.ts
+++ packages/renderer-process/src/parts/DragInfo/DragInfo.ts
@@ -1,9 +1,18 @@
 import { getDragInfo, setDragInfo } from '@lvce-editor/virtual-dom'
 
+const state: { currentDragInfo: any } = {
+  currentDragInfo: undefined,
+}
+
 export const set = (id: string | number, data: any) => {
+  state.currentDragInfo = data
   setDragInfo(id, data)
 }
 
 export const get = (id: string | number) => {
   return getDragInfo(id)
+}
+
+export const getCurrent = (): any => {
+  return state.currentDragInfo
 }
