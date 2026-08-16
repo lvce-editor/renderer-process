1120750cb0903a1a0b2a39a7c9bb3aff55b91227
Le Vivilet <levivilet@pm.me>
2026-08-16 10:46:45 -0700

    feature: forward direct worker commands to renderer worker (#788)

--- packages/renderer-process/src/parts/HandleMessagePort/HandleMessagePort.ts
+++ packages/renderer-process/src/parts/HandleMessagePort/HandleMessagePort.ts
@@ -1,10 +1,16 @@
 import { PlainMessagePortRpcParent } from '@lvce-editor/rpc'
 import * as DirectViewRpcRegistry from '../DirectViewRpcRegistry/DirectViewRpcRegistry.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 import * as Viewlet from '../Viewlet/Viewlet.ts'
+
+const forwardRendererWorkerCommand = (method: string, ...params: readonly unknown[]): void => {
+  RendererWorker.send(method, ...params)
+}
 
 export const handleMessagePort = async (port: MessagePort, rpcId?: string): Promise<void> => {
   const rpc = await PlainMessagePortRpcParent.create({
     commandMap: {
+      'Viewlet.forwardRendererWorkerCommand': forwardRendererWorkerCommand,
       'Viewlet.queueCommands': Viewlet.queueCommands,
     },
     messagePort: port,
