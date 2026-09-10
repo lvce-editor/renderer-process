5cf0492f2d72c7e3bcf041b94349181323732f3c
Le Vivilet <levivilet@pm.me>
2026-09-10 16:33:56 +0200

    feature: accept numeric parsed selector types (#868)
    * feature: accept numeric parsed selector types
    
    * fix: narrow numeric nth selector when printing

--- packages/renderer-process/src/parts/TestFrameWork/ParsedCssSelector.ts
+++ packages/renderer-process/src/parts/TestFrameWork/ParsedCssSelector.ts
@@ -1,19 +1,21 @@
+import type * as SelectorType from '../SelectorType/SelectorType.ts'
+
 type ParsedSelectorPart =
   | {
       readonly selector: string
-      readonly type: 'css'
+      readonly type: typeof SelectorType.Css | 'css'
     }
   | {
       readonly text: string
-      readonly type: 'text'
+      readonly type: typeof SelectorType.Text | 'text'
     }
   | {
       readonly text: string
-      readonly type: 'has-text'
+      readonly type: typeof SelectorType.HasText | 'has-text'
     }
   | {
       readonly index: number
-      readonly type: 'nth'
+      readonly type: typeof SelectorType.Nth | 'nth'
     }
 
 export type ParsedCssSelector = readonly ParsedSelectorPart[]
